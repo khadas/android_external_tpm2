773d241a90fcb5d3648151cb5982e5f0815369cb
Vadim Bendebury <vbendeb@chromium.org>
2015-05-29 16:55:27 -0700

    Changes to allow compilation of DictionaryAttackLockReset.c
    BUG=none
    TEST=compilation succeeds:
       cc -Wall -Werror -c -o /dev/null DictionaryAttackLockReset.c
    
    Change-Id: I784178fef04d64bfc93559c0023965c5b09938db
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/274009
    Reviewed-by: Utkarsh Sanghi <usanghi@chromium.org>

---
+++ DictionaryAttackLockReset_fp.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __TPM2_DICTIONARYATTACKLOCKRESET_FP_H
+#define __TPM2_DICTIONARYATTACKLOCKRESET_FP_H
+
+typedef void  DictionaryAttackLockReset_In; // Unused.
+
+#endif // __TPM2_DICTIONARYATTACKLOCKRESET_FP_H
