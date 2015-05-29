8ec80179a117a0894e4b9a79c17ce898730c0d94
Vadim Bendebury <vbendeb@chromium.org>
2015-05-29 13:55:43 -0700

    Changes to allow compilation of CryptSelfTest.c
    BUG=none
    TEST=compilation succeeds:
       cc -Wall -Werror -c -o /dev/null CryptSelfTest.c
    
    Change-Id: Ia068ffcde577b3cceb1ccd11088fac0be1ca97b6
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/274006
    Reviewed-by: Utkarsh Sanghi <usanghi@chromium.org>

---
+++ CryptSelfTest_fp.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __TPM2_CRYPTSELFTEST_FP_H
+#define __TPM2_CRYPTSELFTEST_FP_H
+
+TPM_RC CryptTestAlgorithm(
+        TPM_ALG_ID                 alg,
+        ALGORITHM_VECTOR          *toTest
+);
+
+#endif // __TPM2_CRYPTSELFTEST_FP_H
