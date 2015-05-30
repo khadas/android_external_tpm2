06146759b6b8dd2d8915a6524377d668c54241d6
Vadim Bendebury <vbendeb@chromium.org>
2015-05-29 20:57:35 -0700

    Changes to allow compilation of DictionaryAttackParameters.c
    BUG=none
    TEST=compilation succeeds:
       cc -Wall -Werror -c -o /dev/null DictionaryAttackParameters.c
    
    Change-Id: I32fdf477214ec1bb73d2be37ec2619bc9377bde0
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/274170
    Reviewed-by: Utkarsh Sanghi <usanghi@chromium.org>

---
+++ DictionaryAttackParameters_fp.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __TPM2_DICTIONARYATTACKPARAMETERS_FP_H
+#define __TPM2_DICTIONARYATTACKPARAMETERS_FP_H
+
+typedef struct {
+        UINT32  newMaxTries;    // number of unexpired authorization
+                                // failures before the TPM is in
+                                // lockout
+        UINT32  newRecoveryTime;// time between authorization failures
+                                // before failedTries is decremented
+        UINT32  lockoutRecovery;// time that must expire between
+} DictionaryAttackParameters_In;
+
+TPM_RC TPM2_DictionaryAttackParameters(
+        DictionaryAttackParameters_In    *in             // IN: input parameter list
+);
+
+#endif // __TPM2_DICTIONARYATTACKPARAMETERS_FP_H
