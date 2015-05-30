b6ce88abd3985803cba600c6de4c57089fcce6a6
Vadim Bendebury <vbendeb@chromium.org>
2015-05-29 23:04:30 -0700

    Changes to allow compilation of HierarchyChangeAuth.c
    BUG=none
    TEST=compilation succeeds:
       cc -Wall -Werror -c -o /dev/null HierarchyChangeAuth.c
    
    Change-Id: I14845f7e98989b0c1cfb6d04b122367f574b5a30
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/274195
    Reviewed-by: Utkarsh Sanghi <usanghi@chromium.org>

---
+++ HierarchyChangeAuth_fp.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __TPM2_HIERARCHYCHANGEAUTH_FP_H
+#define __TPM2_HIERARCHYCHANGEAUTH_FP_H
+
+typedef struct {
+        TPM_RH     authHandle;
+        TPM2B_AUTH newAuth;
+} HierarchyChangeAuth_In;
+
+TPM_RC TPM2_HierarchyChangeAuth(
+        HierarchyChangeAuth_In    *in                    // IN: input parameter list
+                                );
+
+#endif // __TPM2_HIERARCHYCHANGEAUTH_FP_H
