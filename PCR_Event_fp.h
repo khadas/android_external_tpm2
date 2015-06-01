50ca31ab942e1f2182a5751cac119d8c61cbfd0b
Vadim Bendebury <vbendeb@chromium.org>
2015-06-01 11:45:15 -0700

    Changes to allow compilation of PCR_Event.c
    BUG=none
    TEST=compilation succeeds:
       cc -Wall -Werror -c -o /dev/null PCR_Event.c
    
    Change-Id: I543ffe60d0d865389eb47775d2779440f7f98ec4
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/274623
    Reviewed-by: Utkarsh Sanghi <usanghi@chromium.org>

---
+++ PCR_Event_fp.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __TPM2_PCR_EVENT_FP_H_
+#define __TPM2_PCR_EVENT_FP_H_
+
+typedef struct {
+        TPM_RH	   pcrHandle;
+        TPM2B_DATA eventData;
+} PCR_Event_In;
+
+typedef struct {
+        TPML_DIGEST  digests;
+} PCR_Event_Out;
+
+TPM_RC TPM2_PCR_Event(
+        PCR_Event_In      *in,             // IN: input parameter list
+        PCR_Event_Out     *out             // OUT: output parameter list
+);
+
+#endif // __TPM2_PCR_EVENT_FP_H_
