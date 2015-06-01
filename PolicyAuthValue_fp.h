4be4cee7974ea0ceaf97ae42c218583d69de88c1
Vadim Bendebury <vbendeb@chromium.org>
2015-06-01 12:30:16 -0700

    Changes to allow compilation of PolicyAuthValue.c
    BUG=none
    TEST=compilation succeeds:
       cc -Wall -Werror -c -o /dev/null PolicyAuthValue.c
    
    Change-Id: Id61643561b5726323240694ad97baef309fa38a4
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/274631
    Reviewed-by: Utkarsh Sanghi <usanghi@chromium.org>

---
+++ PolicyAuthValue_fp.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __TPM2_POLICYAUTHVALUE_FP_H_
+#define __TPM2_POLICYAUTHVALUE_FP_H_
+
+typedef struct {
+        TPM_HANDLE  policySession;
+} PolicyAuthValue_In;
+
+TPM_RC TPM2_PolicyAuthValue(
+        PolicyAuthValue_In    *in            // IN: input parameter list
+);
+
+#endif // __TPM2_POLICYAUTHVALUE_FP_H_
