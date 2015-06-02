88eaf02b4ff62ee6ca4faf2ef7e2181b14219fd4
Vadim Bendebury <vbendeb@chromium.org>
2015-06-01 18:22:47 -0700

    Changes to allow compilation of SetAlgorithmSet.c
    BUG=none
    TEST=compilation succeeds:
       cc -Wall -Werror -c -o /dev/null SetAlgorithmSet.c
    
    Change-Id: I4e1f2654a4255bac792bb09720981d1f65fdf948
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/274663
    Reviewed-by: Utkarsh Sanghi <usanghi@chromium.org>

---
+++ SetAlgorithmSet_fp.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __TPM2_SETALGORITHMSET_FP_H_
+#define __TPM2_SETALGORITHMSET_FP_H_
+
+typedef struct {
+        UINT32  algorithmSet;
+} SetAlgorithmSet_In;
+
+TPM_RC TPM2_SetAlgorithmSet(
+        SetAlgorithmSet_In     *in            // IN: input parameter list
+);
+
+#endif // __TPM2_SETALGORITHMSET_FP_H_
