775f3b3a96b24ae205179d885d49efd3fd9607cd
Vadim Bendebury <vbendeb@chromium.org>
2015-06-01 17:41:33 -0700

    Changes to allow compilation of ReadPublic.c
    BUG=none
    TEST=compilation succeeds:
       cc -Wall -Werror -c -o /dev/null ReadPublic.c
    
    Change-Id: I3d7db90464fccf6c283e19a41cca5cecf7cdd182
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/274656
    Reviewed-by: Utkarsh Sanghi <usanghi@chromium.org>

---
+++ ReadPublic_fp.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2015 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __TPM2_READPUBLIC_FP_H_
+#define __TPM2_READPUBLIC_FP_H_
+
+typedef struct {
+        TPMI_DH_OBJECT objectHandle;
+} ReadPublic_In;
+
+typedef struct {
+        TPM2B_PUBLIC outPublic;
+        TPM2B_NAME   name;
+        TPM2B_NAME   qualifiedName;
+} ReadPublic_Out;
+
+TPM_RC TPM2_ReadPublic(
+        ReadPublic_In     *in,                // IN: input parameter list
+        ReadPublic_Out    *out                // OUT: output parameter list
+);
+
+#endif // __TPM2_READPUBLIC_FP_H_
