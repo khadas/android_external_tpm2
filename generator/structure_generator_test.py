595736f2120e594839d6e73cb8245d8d7e5c2360
Vadim Bendebury <vbendeb@chromium.org>
2015-09-17 17:19:18 -0700

    generator: modify the scripts to process HTML sources
    The scripts scraping TPM2 specification are being changed to use the
    specification directly (in HTML forms). The existing modules
    (command_generator and structure_generator) are being modified to
    provide the 'back end processing' for the data retrieved by the new
    scraper (extract_structures and tpm_table).
    
    The test is modified to still pass, test_structure_generator.html is
    the source of test data.
    
    README includes instructions how to re-generate the source code.
    
    BUG=chromium:501639
    TEST=new code generation works fine, the test also passes:
       $ ./generator/structure_generator_test.py
       .......
       ----------------------------------------------------------------------
       Ran 7 tests in 0.007s
    
       OK
    
    Change-Id: I2a0e925475d4cd2b57528bfe1b162245fc6e0f43
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/300662

--- generator/structure_generator_test.py
+++ generator/structure_generator_test.py
@@ -9,8 +9,10 @@
 from __future__ import print_function
 
 import StringIO
+import os
 import unittest
 
+import extract_structures
 import structure_generator
 
 
@@ -78,18 +80,22 @@ class TestGenerators(unittest.TestCase):
     """Test generation of marshaling code for structures."""
     marshalled_types = set(['int'])
     struct = structure_generator.Structure('TEST_STRUCT')
-    struct.AddField('UINT16', 'type', None, False)
-    struct.AddField('TPMI_TYPE', 'interfaceField0', 'TRUE', False)
-    struct.AddField('TPMI_TYPE', 'interfaceField1', 'FALSE', False)
-    struct.AddField('TPMU_SYM_MODE', 'unionField', 'type', False)
-    struct.AddField('UINT16', 'arrayField', 'MAX_VALUE', True)
+    struct.AddField(structure_generator.Field('UINT16', 'type', None, False))
+    struct.AddField(structure_generator.Field('TPMI_TYPE', 'interfaceField0',
+                                              'TRUE', False))
+    struct.AddField(structure_generator.Field('TPMI_TYPE', 'interfaceField1',
+                                              'FALSE', False))
+    struct.AddField(structure_generator.Field('TPMU_SYM_MODE', 'unionField',
+                                              'type', False))
+    struct.AddField(structure_generator.Field('UINT16', 'arrayField',
+                                              'MAX_VALUE', True))
     typedef = structure_generator.Typedef('int', 'UINT16')
     interface = structure_generator.Interface('UINT16', 'TPMI_TYPE')
     # Choose TPMU_SYM_MODE because it exists in selectors definition and it
     # has few fields.
     union = structure_generator.Union('TPMU_SYM_MODE')
-    union.AddField('UINT16', 'aes', None)
-    union.AddField('UINT16', 'SM4', None)
+    union.AddField(structure_generator.Field('UINT16', 'aes', None))
+    union.AddField(structure_generator.Field('UINT16', 'SM4', None))
     typemap = {
         'UINT16': typedef,
         'TPMI_TYPE': interface,
@@ -107,8 +113,8 @@ class TestGenerators(unittest.TestCase):
     """Test generation of marshaling code for unions."""
     marshalled_types = set(['int'])
     union = structure_generator.Union('TPMU_SYM_MODE')
-    union.AddField('UINT16', 'aes', None)
-    union.AddField('UINT16', 'SM4', None)
+    union.AddField(structure_generator.Field('UINT16', 'aes', None))
+    union.AddField(structure_generator.Field('UINT16', 'SM4', None))
     typedef = structure_generator.Typedef('int', 'UINT16')
     typemap = {'UINT16': typedef}
     out_file = StringIO.StringIO()
@@ -127,87 +133,62 @@ class TestGenerators(unittest.TestCase):
 class TestParser(unittest.TestCase):
   """Test structure parser."""
 
-  FAKE_TYPEDEF = '_BEGIN_TYPES\n_OLD_TYPE uint16_t\n_NEW_TYPE UINT16\n_END\n'
-  FAKE_CONSTANT = ('_BEGIN_CONSTANTS\n_OLD_TYPE base_type\n'
-                   '_NEW_TYPE const_type\n'
-                   '_NAME const_name\n_RETURN return_name\n_END\n')
-  FAKE_ATT_STRUCT = ('_BEGIN_ATTRIBUTE_STRUCTS\n_OLD_TYPE base_type\n'
-                     '_NEW_TYPE att_type\n_RESERVED 4_7\n_RESERVED 9\n_END\n')
-  FAKE_INTERFACE = ('_BEGIN_INTERFACES\n_OLD_TYPE base_type\n'
-                    '_NEW_TYPE interface_type\n_NAME const_name\n'
-                    '_MIN min_name\n_MAX max_name\n_CONDITIONAL null_name\n'
-                    '_RETURN return_name\n_END\n')
-  FAKE_STRUCTURE = ('_BEGIN_STRUCTURES\n_STRUCTURE struct_type\n_TYPE UINT16\n'
-                    '_NAME field1\n_MIN field1 min\n_TYPE UINT16\n'
-                    '_NAME field2\n_TYPE UINT16\n_NAME field3 _ARRAY field1\n'
-                    '_MAX field1 max\n'
-                    '_TYPE UINT16\n_NAME field4 _UNION field2\n'
-                    '_TYPE interface_type\n_NAME field5 _PLUS\n_END\n')
-  FAKE_UNION = ('_BEGIN_UNIONS\n_UNION union_type\n_TYPE field1_type\n'
-                '_NAME field1\n_TYPE field2_type\n_NAME field2\n_END\n')
-
-  def testStructureParserWithBadData(self):
-    """Test the structure parser with invalid data."""
-    input_data = 'bad_data'
-    in_file = StringIO.StringIO(input_data)
-    parser = structure_generator.StructureParser(in_file)
-    typemap = parser.Parse()
-    types = typemap.values()
-    self.assertIsNotNone(types)
-    self.assertIsNotNone(typemap)
-
   def testStructureParser(self):
     """Test the structure parser with valid data.
 
        One of each typedef, constant type, attribute structure, interface,
        structure, and union. Should appear in types array in that order.
     """
-    input_data = (self.FAKE_TYPEDEF + self.FAKE_CONSTANT +
-                  self.FAKE_ATT_STRUCT + self.FAKE_INTERFACE +
-                  self.FAKE_STRUCTURE + self.FAKE_UNION)
-    in_file = StringIO.StringIO(input_data)
-    parser = structure_generator.StructureParser(in_file)
-    typemap = parser.Parse()
-    types = [typemap[x] for x in sorted(typemap.keys())]
+    html_parser = extract_structures.SpecParser()
+    html_file_name = os.path.join(os.path.dirname(__file__),
+                                  'test_structure_generator.html')
+    html_parser.feed(open(html_file_name).read())
+    html_parser.close()
+    types = html_parser.GetTable().GetTypeMap()
     self.assertEqual(len(types), 6)
-    self.assertEqual(types[0].old_type, 'uint16_t')
-    self.assertEqual(types[0].new_type, 'UINT16')
-    self.assertEqual(types[1].old_type, 'base_type')
-    self.assertEqual(types[1].new_type, 'att_type')
-    self.assertEqual(types[1].reserved[0], '4_7')
-    self.assertEqual(types[1].reserved[1], '9')
-    self.assertEqual(types[2].old_type, 'base_type')
-    self.assertEqual(types[2].new_type, 'const_type')
-    self.assertEqual(types[2].valid_values[0], 'const_name')
-    self.assertEqual(types[2].error_code, 'return_name')
-    self.assertEqual(types[3].old_type, 'base_type')
-    self.assertEqual(types[3].new_type, 'interface_type')
-    self.assertEqual(types[3].bounds[0][0], 'min_name')
-    self.assertEqual(types[3].bounds[0][1], 'max_name')
-    self.assertEqual(types[3].valid_values[0], 'const_name')
-    self.assertEqual(types[3].conditional_value, 'null_name')
-    self.assertEqual(types[3].error_code, 'return_name')
-    self.assertEqual(types[4].name, 'struct_type')
-    self.assertEqual(types[4].fields[0].field_type, 'UINT16')
-    self.assertEqual(types[4].fields[0].field_name, 'field1')
-    self.assertEqual(types[4].fields[1].field_type, 'UINT16')
-    self.assertEqual(types[4].fields[1].field_name, 'field2')
-    self.assertEqual(types[4].fields[2].field_type, 'UINT16')
-    self.assertEqual(types[4].fields[2].field_name, 'field3')
-    self.assertEqual(types[4].fields[2].extra_argument, 'field1')
-    self.assertEqual(types[4].fields[3].field_type, 'UINT16')
-    self.assertEqual(types[4].fields[3].field_name, 'field4')
-    self.assertEqual(types[4].fields[3].extra_argument, 'field2')
-    self.assertEqual(types[4].fields[4].field_type, 'interface_type')
-    self.assertEqual(types[4].fields[4].field_name, 'field5')
-    self.assertEqual(types[4].fields[4].extra_argument, 'TRUE')
-    self.assertEqual(types[4].upper_bounds['field1'], 'max')
-    self.assertEqual(types[4].lower_bounds['field1'], 'min')
-    self.assertEqual(types[5].name, 'union_type')
-    self.assertEqual(types[5].fields[0].field_type, 'field1_type')
-    self.assertEqual(types[5].fields[0].field_name, 'field1')
-    self.assertEqual(types[5].fields[1].field_type, 'field2_type')
-    self.assertEqual(types[5].fields[1].field_name, 'field2')
+    tpm_obj = types['UINT16']
+    self.assertEqual(tpm_obj.old_type, 'uint16_t')
+    self.assertEqual(tpm_obj.new_type, 'UINT16')
+    tpm_obj = types['TPMA_LOCALITY']
+    self.assertEqual(tpm_obj.old_type, 'base_type')
+    self.assertEqual(tpm_obj.new_type, 'TPMA_LOCALITY')
+    self.assertEqual(tpm_obj.reserved[0], '4_7')
+    self.assertEqual(tpm_obj.reserved[1], '9')
+    tpm_obj = types['const_type']
+    self.assertEqual(tpm_obj.old_type, 'base_type')
+    self.assertEqual(tpm_obj.new_type, 'const_type')
+    self.assertEqual(tpm_obj.valid_values[0], 'const_name')
+    self.assertEqual(tpm_obj.error_code, 'return_name')
+    tpm_obj = types['TPMI_DH_OBJECT']
+    self.assertEqual(tpm_obj.old_type, 'base_type')
+    self.assertEqual(tpm_obj.new_type, 'TPMI_DH_OBJECT')
+    self.assertEqual(tpm_obj.bounds[0][0], 'min_name')
+    self.assertEqual(tpm_obj.bounds[0][1], 'max_name')
+    self.assertEqual(tpm_obj.valid_values[0], 'const_name')
+    self.assertEqual(tpm_obj.conditional_value, 'null_name')
+    self.assertEqual(tpm_obj.error_code, 'return_name')
+    tpm_obj = types['struct_type']
+    self.assertEqual(tpm_obj.name, 'struct_type')
+    self.assertEqual(tpm_obj.fields[0].field_type, 'UINT16')
+    self.assertEqual(tpm_obj.fields[0].field_name, 'field1')
+    self.assertEqual(tpm_obj.fields[1].field_type, 'UINT16')
+    self.assertEqual(tpm_obj.fields[1].field_name, 'field2')
+    self.assertEqual(tpm_obj.fields[2].field_type, 'UINT16')
+    self.assertEqual(tpm_obj.fields[2].field_name, 'field3')
+    self.assertEqual(tpm_obj.fields[2].run_time_size, 'field1')
+    self.assertEqual(tpm_obj.fields[3].field_type, 'UINT16')
+    self.assertEqual(tpm_obj.fields[3].field_name, 'field4')
+    self.assertEqual(tpm_obj.fields[3].selector_value, 'field2')
+    self.assertEqual(tpm_obj.fields[4].field_type, 'interface_type')
+    self.assertEqual(tpm_obj.fields[4].field_name, 'field5')
+    self.assertEqual(tpm_obj.upper_bounds['field1'], 'max')
+    self.assertEqual(tpm_obj.lower_bounds['field1'], 'min')
+    tpm_obj = types['union_type']
+    self.assertEqual(tpm_obj.name, 'union_type')
+    self.assertEqual(tpm_obj.fields[0].field_type, 'field1_type')
+    self.assertEqual(tpm_obj.fields[0].field_name, 'field1')
+    self.assertEqual(tpm_obj.fields[1].field_type, 'field2_type')
+    self.assertEqual(tpm_obj.fields[1].field_name, 'field2')
 
 if __name__ == '__main__':
   unittest.main()
